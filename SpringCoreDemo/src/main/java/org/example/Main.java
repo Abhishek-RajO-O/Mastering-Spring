1c5ebec1b1bae483fee16f799043ab31564c3039
Abhishek Raj <abhishekraj605912@gmail.com>
2026-07-24 03:12:28 +0530

    Lecture 4 half way

--- SpringCoreDemo/src/main/java/org/example/Main.java
+++ SpringCoreDemo/src/main/java/org/example/Main.java
@@ -1,10 +1,49 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main(String[] args) {
-        OrderService order = new OrderService();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        OrderService order = context.getBean(OrderService.class);
         order.placeOrder();
+
+//        PaymentService paymentService = new PaymentService();
+//        OrderService order = new OrderService(paymentService);
+//        order.placeOrder();
+
+//        Class<Student> c1 = Student.class;
     }
+    /*
+    Reflection API
+
+    class name -Student
+    fields - name, age
+    Constructor - Student()
+    Methods -> getAttendence() , print()
+    Annotations
+     */
+    /*
+
+    class  Student{
+        private String name;
+        private  int age;
+
+        public Student(){
+
+        }
+
+        public void getAttendence(){
+
+        }
+        public void print(){
+
+        }
+    }
+
+     */
 }
