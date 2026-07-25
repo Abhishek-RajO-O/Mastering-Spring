36839dbb18acb34ddcfa0222607a7875049b0885
Abhishek Raj <abhishekraj605912@gmail.com>
2026-07-25 23:54:25 +0530

    From class to Interface Payment Methods

--- SpringCoreDemo/src/main/java/org/example/PaymentService.java
+++ SpringCoreDemo/src/main/java/org/example/PaymentService.java
@@ -1,10 +1,4 @@
 package org.example;
 
-import org.springframework.stereotype.Component;
-
-@Component
-public class PaymentService {
-    public void  pay(){
-        System.out.println("Payment Done");
-    }
+public interface PaymentService {
 }
