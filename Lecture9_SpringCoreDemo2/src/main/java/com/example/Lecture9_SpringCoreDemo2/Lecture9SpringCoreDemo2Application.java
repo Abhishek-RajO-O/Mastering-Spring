25297afbb605675ce44d6496eed48289a3f044b0
Abhishek Raj <abhishekraj605912@gmail.com>
2026-08-02 20:07:30 +0530

    Spring Boot core

--- Lecture9_SpringCoreDemo2/src/main/java/com/example/Lecture9_SpringCoreDemo2/Lecture9SpringCoreDemo2Application.java
+++ Lecture9_SpringCoreDemo2/src/main/java/com/example/Lecture9_SpringCoreDemo2/Lecture9SpringCoreDemo2Application.java
@@ -2,6 +2,7 @@ package com.example.Lecture9_SpringCoreDemo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Lecture9SpringCoreDemo2Application {
@@ -9,5 +10,8 @@ public class Lecture9SpringCoreDemo2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Lecture9SpringCoreDemo2Application.class, args);
 	}
-
+	@Bean
+	public UserService getUser(){
+		return new UserService();
+	}
 }
--- Lecture9_SpringCoreDemo2/src/main/java/com/example/Lecture9_SpringCoreDemo2/UserService.java
+++ Lecture9_SpringCoreDemo2/src/main/java/com/example/Lecture9_SpringCoreDemo2/UserService.java
@@ -1,4 +1,7 @@
 package com.example.Lecture9_SpringCoreDemo2;
 
 public class UserService {
+    public UserService() {
+        System.out.println("User created");
+    }
 }
